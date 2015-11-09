65e7c487d5081446f91e021ee3a204ba5986ffab
U-DESKTOP-G3OEO3N\kakato10 <svetimir.ignatov@gmail.com>
2015-11-09 17:42:55 +0200

    Adding code written at the exercise

---
+++ 6_09.11.2015/array_sorting.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int ARRAY_SIZE = 4;
+	int array[ARRAY_SIZE];
+
+	//adding values for every element in the array
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		cout << "Please insert an integer value: ";
+		cin >> array[i];
+	}
+
+	for (int i = 0; i < ARRAY_SIZE - 1; i++) {
+		for (int j = i + 1; j < ARRAY_SIZE; j++) {
+			//check if the array is an ascending sequence
+			if (array[i] > array[j]) {
+				//swaping values that are breaking ascendence
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+
+	//visualizing the array at the end
+	for (int i = 0; i < ARRAY_SIZE; i++)
+		cout << "Number[" << i << "] is: : "<< array[i] << endl;
+
+	system("pause");
+	return 0;
+}
+
